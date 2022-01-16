853a2b876d1be332e0326c5956aa8b6cbcbc8f29
DangerCow <kaydeencoded@gmail.com>
2022-01-16 15:53:29 -0700

    Landing page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,99 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import p5 from 'p5'
+import styles from './styles.css'
 
-class HelloMessage extends React.Component {
+var canvas
+
+class SketchWindow extends React.Component{
+  constructor(props){
+    super(props)
+    this.myRef = React.createRef()
+  }
+
+  Sketch = (p) => {
+    let points = []
+
+    let x = p.windowWidth / 2
+    let y = p.windowHeight / 2
+
+    let t = 0
+    let drawTimer = 0;
+    let drawTimerLength = 2;
+    let noiseScale = 4;
+
+    p.windowResized = () => {
+      //resize the canvas if the window is resized
+      p.resizeCanvas(p.windowWidth, p.windowHeight)
+    }
+
+    p.setup = () => {
+      //create background canvas
+      canvas = p.createCanvas(p.windowWidth, p.windowHeight)
+      canvas.position(0, 0)
+      canvas.style('z-index', '-1')
+    }
+
+    p.draw = () => {
+      let colorA = p.color(152, 255, 152);
+      let colorB = p.color(252, 188, 184);
+      let color = p.lerpColor(colorA, colorB, (p.sin(t) + 1) / 2)
+      p.background(color)
+
+      //move xy
+      p.noiseDetail(2, 0.2)
+
+      let noiseX = p.noise(t * noiseScale, 0)
+      let noiseY = p.noise(0, t * noiseScale)
+
+      x = p.sin(noiseX * 2) * p.windowWidth
+      y = p.sin(noiseY * 2) * p.windowHeight
+
+      let currentPoint = [x, y]
+
+      //if there are no points in the list add a default one so nothing breaks :)
+      if (points.length == 0) points.push(currentPoint)
+      
+      //if the draw timer is up add current position as new point
+      if (drawTimer >= drawTimerLength){
+        drawTimer = 0
+        points.push(currentPoint)
+      }
+
+      //draw points
+      for (let i = 1; i < points.length + 1; i++) {
+        let lastPoint = points[i - 1]
+        let point = currentPoint
+
+        if(i != points.length) point = points[i]
+        
+        p.stroke(p.lerpColor(colorA, colorB, (p.sin(t * -1) + 1) / 2))
+        p.strokeWeight(p.abs(p.sin(t)) * 4)
+        p.line(lastPoint[0], lastPoint[1], point[0], point[1])
+      }
+
+      drawTimer += 1
+      t += 0.01
+    }
+  }
+
+  componentDidMount() {
+    this.myP5 = new p5(this.Sketch, this.myRef.current)
+  }
+
   render() {
     return (
-      <div>
-        Hello world..
+      <div ref = {this.myRef}>
+        <div className = 'Center'>
+          <h1>DANGER COW</h1>
+          <p>Website under construction :)</p>
+        </div>
       </div>
-    );
+    )
   }
 }
 
 ReactDOM.render(
-  <HelloMessage name="" />,
+  <SketchWindow name="" />,
   document.getElementById('root')
 );
